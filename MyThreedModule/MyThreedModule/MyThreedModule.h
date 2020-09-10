85877fa7604ea9a600d6b28a8e44958a0466ac66
wangqiang146 <wangqiang18@jd.com>
2020-09-10 18:16:20 +0800

    add pod files

---
+++ MyThreedModule/MyThreedModule/MyThreedModule.h
@@ -0,0 +1,19 @@
+//
+//  MyThreedModule.h
+//  MyThreedModule
+//
+//  Created by 王强 on 2020/9/10.
+//  Copyright © 2020 王强. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for MyThreedModule.
+FOUNDATION_EXPORT double MyThreedModuleVersionNumber;
+
+//! Project version string for MyThreedModule.
+FOUNDATION_EXPORT const unsigned char MyThreedModuleVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <MyThreedModule/PublicHeader.h>
+
+
